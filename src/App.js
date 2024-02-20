728fbda3d8a4e2e4a3da3c86f3f371678f83400e
Tucker <tucker.kent.strow@gmail.com>
2024-02-20 16:29:50 -0600

    Hello Message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Just like this!
+          Edit <code>src/App.js</code>
+        </p>
+        <br/>
+        <p>
+          Hey Anirudh, Sailekha, Hima, Anu, and Oluwadamilare!
         </p>
         <a
           className="App-link"
